ee044e6e9cd27039a1ed3482576e06a76fafc616
Adam Cegielka <acegielka@griddynamics.com>
2024-11-18 18:58:04 +0100

    refactor: refactor by prettier

--- tests/e2e_JakTestowacPl/lesson_04/login.spec.ts
+++ tests/e2e_JakTestowacPl/lesson_04/login.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('User login to Demobank', () => {
-    const baseUrl = 'https://demo-bank.vercel.app/';
+  const baseUrl = 'https://demo-bank.vercel.app/';
 
   test('successful login with too short username', async ({ page }) => {
     await page.goto(baseUrl);
@@ -19,7 +19,9 @@ test.describe('User login to Demobank', () => {
     await page.getByTestId('password-input').click();
 
     // Assert
-    await expect(page.getByTestId('error-login-id')).toHaveText('identyfikator ma min. 8 znaków');
+    await expect(page.getByTestId('error-login-id')).toHaveText(
+      'identyfikator ma min. 8 znaków',
+    );
   });
 
   test('unsuccessful login with too short password', async ({ page }) => {
@@ -29,7 +31,9 @@ test.describe('User login to Demobank', () => {
     await page.getByTestId('password-input').blur();
 
     // Assert
-    await expect(page.getByTestId('error-login-password')).toHaveText('hasło ma min. 8 znaków');
+    await expect(page.getByTestId('error-login-password')).toHaveText(
+      'hasło ma min. 8 znaków',
+    );
   });
 });
 
--- tests/e2e_JakTestowacPl/lesson_05/login.spec.ts
+++ tests/e2e_JakTestowacPl/lesson_05/login.spec.ts
@@ -30,7 +30,9 @@ test.describe('User login to Demobank', () => {
     await page.getByTestId('password-input').click();
 
     // Assert
-    await expect(page.getByTestId('error-login-id')).toHaveText(expectedErrorLogin);
+    await expect(page.getByTestId('error-login-id')).toHaveText(
+      expectedErrorLogin,
+    );
   });
 
   test('unsuccessful login with too short password', async ({ page }) => {
@@ -46,7 +48,9 @@ test.describe('User login to Demobank', () => {
     await page.getByTestId('password-input').blur();
 
     // Assert
-    await expect(page.getByTestId('error-login-password')).toHaveText(expectedErrorPassword);
+    await expect(page.getByTestId('error-login-password')).toHaveText(
+      expectedErrorPassword,
+    );
   });
 });
 
